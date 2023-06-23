4bc67b7ede696b6cc97d5aa00f4f8d561e96271e
Jing <111691710+J0JIng@users.noreply.github.com>
2023-06-23 20:18:25 +0800

    Update server_sv_manager.py

--- code/server_sv_manager.py
+++ code/server_sv_manager.py
@@ -13,7 +13,7 @@ import aiocoap
 from aiocoap import *
 from aiocoap.protocol import Request
 
-# For Network 
+# For Network
 import socket
 from zeroconf import ServiceInfo
 from zeroconf import ServiceBrowser, ServiceListener, Zeroconf
@@ -94,21 +94,32 @@ class ServerManager:
         """ Returns a dict of all children in the sensitivity list. """
         return self.client_ip6
 
+    def allocate_resource(self, ip: IPv6Address):
+        """Allocate a resource to the client based on its EUI64."""
+        # Generate a unique URI for the client based on a slice of its EUI64
+        eui64 = self.client_ip6[ip].eui64
+        resource = str(eui64)[:8]  # Take a slice of the EUI64 to use as the resource
+        self.client_ip6[ip].uri = resource
+        logging.info(f"Allocated resource {resource} to client {ip}")
+
     def update_child_uri(self):
-        """ Add incoming clients to the resource tree. """
+        """Add incoming clients to the resource tree."""
         while len(ServerManager.incoming_queue_child_ips) > 0:
             ip = ServerManager.incoming_queue_child_ips.pop()
 
             if ip not in ServerManager.client_ip6:
                 try:
+                    # Allocate a resource to the client
+                    self.allocate_resource(ip)
+                    logging.info(
+                        str(ip) + " updated in child sensitivity list with resource " + self.client_ip6[ip].uri
+                        
+                    )
                     ServerManager.pend_queue_child_ips.add(ip)
                     logging.info(str(ip) + " added to child pending queue")
-                    tmp_uri = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
-                    ServerManager.client_ip6[ip] = OtDevice(uri=tmp_uri)
-                    logging.info(str(ip) + " updated in child sensitivity list with resource " + tmp_uri)
 
                 except KeyError:
-                    logging.warning("Unable to updated in child sensitivity list with resource ")
+                    logging.warning("Unable to update child sensitivity list with resource")
                     raise ValueError
 
     def update_child_device_info(self, ip: IPv6Address, csv: list):
@@ -132,4 +143,3 @@ class ServerManager:
         except KeyError:
             logging.warning("Child " + str(ip) + " not found in sensitivity list")
             raise ValueError
-
